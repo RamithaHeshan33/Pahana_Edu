a9fddc3fe1681b148f8bf201c8a02dc06ff1fe6a
Ramitha Heshan <142712365+RamithaHeshan33@users.noreply.github.com>
2025-07-28 09:59:11 +0530

    User registration and Welcome Email part is done (#2)
    * CI pipelines for frontend and backend
    
    * project setup
    
    * Create pull_request_template.md
    
    * updated index.jsp
    
    * Admin Registration & Login Process
    
    * category crud and add jakarta mail dependency
    
    * welcome email sending process is done
    
    * Update backend.yml
    
    * Update frontend.yml
    
    * Update backend.yml
    
    * Update frontend.yml

---
+++ Pahana_Edu/DBScript/db.sql
@@ -0,0 +1,46 @@
+-- Create DB
+CREATE DATABASE IF NOT EXISTS pahana_edu;
+
+-- Use DB
+USE pahana_edu;
+
+-- Create users table
+CREATE TABLE IF NOT EXISTS admins (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL
+);
+
+-- Create categories table
+CREATE TABLE IF NOT EXISTS categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    description TEXT
+
+);
+
+-- Create books table
+CREATE TABLE IF NOT EXISTS books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    publisher VARCHAR(100),
+    isbn_number VARCHAR(13) NOT NULL UNIQUE,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    category_id INT NOT NULL,
+    image_path VARCHAR(255),
+    language VARCHAR(50) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+-- Show table structure
+DESCRIBE admins;
+DESCRIBE categories;
+DESCRIBE books;
