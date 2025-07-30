5d0cee5c9f3ab97235b0cff29ad30aab3946aca0
Ramitha Heshan <142712365+RamithaHeshan33@users.noreply.github.com>
2025-07-31 00:27:22 +0530

    Customer CRUD is done in development (#21)
    * CI pipelines for frontend and backend
    
    * project setup
    
    * Create pull_request_template.md
    
    * updated index.jsp
    
    * Admin Registration & Login Process
    
    * category crud and add jakarta mail dependency
    
    * welcome email sending process is done
    
    * Update backend.yml
    
    * Update frontend.yml
    
    * Update backend.yml
    
    * Update frontend.yml
    
    * update the project structure into the 3-tire architecture
    
    * update the 3-tire architecture
    
    * category crud is done
    
    * Delete .github/workflows directory
    
    * Book CRUD is done
    
    * customer CRUD is done

---
+++ Pahana_Edu/src/main/java/org/example/pahana_edu/persistance/customer/model/CustomerModel.java
@@ -0,0 +1,67 @@
+package org.example.pahana_edu.persistance.customer.model;
+
+public class CustomerModel {
+    private Integer customerId;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhone;
+    private String customerAddress;
+    private String customerAccountNumber;
+
+    public CustomerModel(String customerName, String customerEmail, String customerPhone,
+                         String customerAddress, String customerAccountNumber) {
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.customerPhone = customerPhone;
+        this.customerAddress = customerAddress;
+        this.customerAccountNumber = customerAccountNumber;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public String getCustomerAccountNumber() {
+        return customerAccountNumber;
+    }
+
+    public void setCustomerAccountNumber(String customerAccountNumber) {
+        this.customerAccountNumber = customerAccountNumber;
+    }
+}
